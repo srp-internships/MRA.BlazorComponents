383a7776d80fb6b9a10296d668d5f9946e9f9bd6
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2024-02-20 16:21:37 +0500

    Add DialogMessageSende

---
+++ MRA.BlazorComponents/DynamicPages/Services/Message/IMessageService.cs
@@ -0,0 +1,12 @@
+﻿using MRA.BlazorComponents.HttpClient.Responses;
+using MRA.Identity.Application.Contract.Messages.Commands;
+using MRA.Identity.Application.Contract.Messages.Responses;
+
+namespace MRA.BlazorComponents.DynamicPages.Services.Message
+{
+    public interface IMessageService
+    {
+        Task<ApiResponse> SendMessageAsync(SendMessageCommand command);
+        Task<List<GetMessageResponse>> GetAllMessagesAsync();
+    }
+}
